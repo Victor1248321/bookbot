a907135b7b485fd6873051bf5a1afaa4b2eb5f07
Master-Magician <Marvin1248@hotmail.com>
2025-04-26 18:13:16 -0500

    Calculate Amount of Text in Book

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print (text)
+    words = word_count(text)
+    count = 0
+    for word in words:
+        count = count + 1
+    print (f"{count} words found in the documentt")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def word_count(text):
+    count = text.split()
+    return count
 
 main()
