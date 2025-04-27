953d935d14c2a8038cb96f3b2e2f1f70847fd022
Master-Magician <Marvin1248@hotmail.com>
2025-04-26 19:01:39 -0500

    Get Character Count

--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def word_count(text):
     count = text.split()
     return count
+
+def character_count(text):
+    characters = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in characters:
+            characters[lowered] += 1
+        else:
+            characters[lowered] = 1
+    return characters
+
